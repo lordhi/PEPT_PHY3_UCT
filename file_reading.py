744e70d966edc8c3e715c292d2fec710be142fea
robert <robertvandermerwe@gmail.com>
2019-10-03 17:36:47 +0200

    Truncating first and last 5 points automatically

--- file_reading.py
+++ file_reading.py
@@ -1,19 +1,19 @@
 import numpy as np
 
 def read_file(filename):
-	"""Reads a given file and returns the various coordinate numpy arrays."""
-	file = open(filename + ".b", 'r')
-	data = []
-	for i in range(11):
-		data.append([])
-	data = tuple(data)
+    """Reads a given file and returns the various coordinate numpy arrays."""
+    file = open(filename + ".b", 'r')
+    data = []
+    for i in range(11):
+        data.append([])
+    data = list(data)
 
-	file.readline() #headerline
-	for line in file:
-		splitline = list(map(float, line.split()))
-		for i in range(11):
-			data[i].append(splitline[i])
-			
-	data = tuple(map(np.asarray, data))
-
-	return data
+    file.readline() #headerline
+    for line in file:
+        splitline = list(map(float, line.split()))
+        for i in range(11):
+            data[i].append(splitline[i]) 
+    for j in range(11):
+        data[j] = data[j][5:-5]
+    data = tuple(map(np.asarray, data))
+    return data
