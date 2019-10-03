b629e86b221fdfa38c4e9d6e0ff7f84f35f6583b
robert <robertvandermerwe@gmail.com>
2019-10-03 22:25:46 +0200

    Added parameter to exclude boundaries

--- file_reading.py
+++ file_reading.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def read_file(filename):
+def read_file(filename, exclude_boundary = 0):
     """Reads a given file and returns the various coordinate numpy arrays."""
     file = open(filename + ".b", 'r')
     data = []
@@ -15,5 +15,12 @@ def read_file(filename):
             data[i].append(splitline[i]) 
     for j in range(11):
         data[j] = data[j][5:-5]
-    data = tuple(map(np.asarray, data))
+
+    data = list(map(np.asarray, data))
+    if exclude_boundary > 0:
+        minz = np.min(data[3])
+        maxz = np.max(data[3])
+        a = data[3]
+        for i in range(len(data)):
+            data[i] = data[i][np.logical_and(np.greater(a, minz+exclude_boundary), np.less(a, maxz - exclude_boundary))]
     return data
