cb56ec32a264830e0d428d2620cb84069a6e7647
robert <robertvandermerwe@gmail.com>
2019-10-04 22:39:09 +0200

    Finalised calibration speeds and outputs

--- velocity_calibration.py
+++ velocity_calibration.py
@@ -3,14 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-###Things I still want to check - How to handle uncertainties on speed - 10%/standard deviation/propagate
-###Should I make Gaussian fit to histogram or is mean good enough
-
 filenames = ["../Data/LSpeed","../Data/MSpeed","../Data/HSpeed"]
-p0s = [[1.7, 10, 0.06,-0.005],[3.2,10,0.06,-0.005],[5.1,10,0.06,-0.005]]
-
-#def gauss(x, A, sigma, mu):
-#    return (A/(sigma*np.sqrt(2*np.pi)))*np.exp((-1/2)*np.square((x-mu)/sigma))
+angular_p0s = [[1.7, 10, 0.06,-0.005],[3.2,10,0.06,-0.005],[5.1,10,0.06,-0.005]]
 
 def f(t,omega, delta, a, c):
     return a*np.sin(omega*t+delta) + c
@@ -18,111 +12,67 @@ def f(t,omega, delta, a, c):
 def magnitude(v1,v2,v3):
     return np.sqrt(np.square(v1) + np.square(v2) + np.square(v3))
 
-#def fit(func, x,y, u, p0):
-#    popt, pcov = curve_fit(func, x, y, p0,u, absolute_sigma = True)
-#        
-#    residuals = y-func(x,*popt)
-#    dymin = residuals/u
-#    ss_res = np.sum(np.square(residuals))
-#    ss_tot = np.sum((y-np.mean(y))**2)
-#    r_squared = 1-(ss_res/ss_tot)
-#    min_chisq = sum(dymin*dymin)
-#    dof = len(x) - len(popt)
-    
-#    return popt, pcov, min_chisq/dof, r_squared
 
-def findLinearVelocity(t,vx,vy,vz):
-    v_mag = magnitude(vx,vy,vz)
-    plt.figure()
-    plt.xlabel("t (ms)")
-    plt.ylabel("Linear velocity (m/s)")
-    plt.plot(t, v_mag, 'bo', markersize = 0.3, alpha = 0.3)
-    mean = np.mean(v_mag)
-    uvmag = np.std(v_mag)
+def fit(func, x,y,  p0, names, u=None):
+    if u is not None:
+        popt, pcov = curve_fit(func, x, y, p0,u, absolute_sigma = True)
+        residuals = y-func(x,*popt)
+        dymin = residuals/u
+    else:
+        popt, pcov = curve_fit(func, x, y, p0, absolute_sigma = True)
+        residuals = y-func(x,*popt)
+        dymin = residuals
     
-    
-    print("Mean:", mean, " m/s - Standard deviation:", uvmag, "m/s")
-    
-    plt.plot(t, np.ones(t.shape) * mean)
-    plt.plot(t, np.ones(t.shape) * (mean+uvmag))
-    plt.plot(t, np.ones(t.shape) * (mean-uvmag))
-
-def findAngularSpeed(t,x,u, p0):
-    t = t[400:800]/1000
-    x = x[400:800]/1000
-    u = u[400:800]/1000
-    
-    plt.figure()
-    plt.plot(t,x, 'ro', markersize = 0.5)
-    
-    names = ["omega", "delta", "a", 'c']
-    popt, pcov = curve_fit(f, t, x,  p0, u,  absolute_sigma = True)
-    
-    residuals = x-f(t,*popt)
-    dymin = residuals/u
     ss_res = np.sum(np.square(residuals))
-    ss_tot = np.sum((x-np.mean(t))**2)
+    ss_tot = np.sum((y-np.mean(y))**2)
     r_squared = 1-(ss_res/ss_tot)
     min_chisq = sum(dymin*dymin)
-    dof = len(t) - len(popt)
+    dof = len(x) - len(popt)
     print('Chi square', min_chisq)
     print('Number of degrees of freedom: ', dof)
     print('Chi square per degrees of freedom: ', min_chisq/dof)
     print("R^2: ", r_squared )
     print()
     
-    print()
     print('Fitted parameters with 68% C.I.:')
     
     for i, pmin in enumerate(popt):
-        print("%2i %-10s %12f +/- %10f" %(i, names[i], pmin, np.sqrt(pcov[i,i])*np.sqrt(min_chisq/dof)))
+        print("%2i %-10s %12f +/- %10f" %(i, names[i], pmin, np.sqrt(pcov[i,i])))
     print()
+    return popt
     
+
+def findAngularSpeed(t,x,u, p0):
+    t = t[400:800]/1000  #Select narrow region and convert to seconds
+    x = x[400:800]/1000
+    u = u[400:800]/1000
     
+    names = ["omega", "delta", "a", 'c']
+    popt = fit(f,t,x, p0, names, u = u)
+    
+    ##################################################PLOTTING HERE############################################################
+    plt.figure()
+    plt.title("Angular fit")
+    plt.plot(t,x, 'ro', markersize = 0.5)  #Plots the raw data
     tmodel = np.linspace(t[0], t[-1], 10000)
-    plt.plot(tmodel, f(tmodel, *popt)) 
+    plt.plot(tmodel, f(tmodel, *popt)) #Plots using the returned fit parameters
+
+def findLinearVelocity(t,v_mag):
+    mean = np.mean(v_mag)
+    std = np.std(v_mag)
+    print("Mean:", mean, " m/s - Standard deviation:", std, "m/s")
     
-#def test(t,x,u, p0):
-#    t = np.arange(8,15, 0.01)
-#    x = f(t,2.5,-3,0.06,0)
-#    u = np.ones(t.shape)*0.1*x
-#    plt.plot(t,x)
-#    
-#    names = ["omega", "delta", "a"]
-#    popt, pcov = curve_fit(f, t, x,  p0, u,  absolute_sigma = True)
-#    
-#    residuals = x-f(t,*popt)
-#    dymin = residuals/u
-#    ss_res = np.sum(np.square(residuals))
-#    ss_tot = np.sum((x-np.mean(t))**2)
-#    r_squared = 1-(ss_res/ss_tot)
-#    min_chisq = sum(dymin*dymin)
-#    dof = len(t) - len(popt)
-#    print('Chi square', min_chisq)
-#    print('Number of degrees of freedom: ', dof)
-#    print('Chi square per degrees of freedom: ', min_chisq/dof)
-#    print("R^2: ", r_squared )
-#    print()
-#    
-#    print()
-#    print('Fitted parameters with 68% C.I.:')
-#    
-#    for i, pmin in enumerate(popt):
-#        print("%2i %-10s %12f +/- %10f" %(i, names[i], pmin, np.sqrt(pcov[i,i])*np.sqrt(min_chisq/dof)))
-#    print()
-#    
-#    
-#    tmodel = np.linspace(t[0], t[-1], 10000)
-#    plt.plot(tmodel, f(tmodel, *popt)) 
 
 for i,filename in enumerate(filenames):
     print(filename)
-    print("________________________________")
+    print("______________________________________")
     t,x,y,z,u,vx,vy,vz,uvx,uvy,uvz = read_file(filename)
-    findLinearVelocity(t,vx,vy,vz)
-    findAngularSpeed(t,x,u, p0s[i])
-    print("_________________________________")
-    #test(t,x,u)
+    v_mag = magnitude(vx,vy,vz)
+    print("______________Linear Speed______________")
+    findLinearVelocity(t,v_mag)
+    print("_______________Angular Speed_______________")
+    findAngularSpeed(t,x,u, angular_p0s[i])
+    print("______________________________________")
 
 
 
