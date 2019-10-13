1939f7692e1d07c1dafd3973c549477546ea82ef
WLLKAE001 <wllkae001@uct.ac.za>
2019-10-13 17:57:18 +0200

    Thing

--- energyfluctuation.py
+++ energyfluctuation.py
@@ -2,6 +2,71 @@ from file_reading import read_file
 import matplotlib.pyplot as plt
 import numpy as np
 
+def twoD_binned_energy_fluctuations(x,y,z,vx,vy,vz, zmin, zstep, xmin, xstep):
+	'''Bins data by z and x coordinates. If you need to bin by z and y, simply swap x and y when using the function'''
+	data = list(zip(x,y,z,vx,vy,vz))
+	zmax = max(z)
+	xmax = max(x)
+
+	zbins = []
+	xbins = []
+
+	zbins.append(zmin)
+	xbins.append(xmin)
+
+	while (zbins[-1] < zmax):
+		zbins.append(zbins[-1] + zstep)
+	while (xbins[-1] < xmax):
+		xbins.append(xbins[-1] + xstep)
+
+	zbins = np.asarray(zbins)
+	xbins = np.asarray(xbins)
+
+	binnedData = []
+	for i in range(len(zbins)):
+		binnedData.append([])
+		for j in range(len(xbins)):
+			binnedData[i].append([[],[],[]])
+
+	for i in range(len(binnedData)):
+		for j in range(len(binnedData[i])):
+			binnedData[i][j].append([])
+
+	for i in range(len(data)):
+		a = int((data[i][2] - zmin)/zstep)
+		b = int((data[i][2] - xmin)/xstep)
+
+		if a >= 0 and a < zbins[-1]:
+			if b >= 0 and b < xbins[-1]:
+				val = data[i]
+				binnedData[a][b][0].append(val[3])
+				binnedData[a][b][1].append(val[4])
+				binnedData[a][b][2].append(val[5])
+
+	x_variances = []
+	y_variances = []
+	z_variances = []
+	for i in range(len(binnedData)):
+		x_variances.append([])
+		y_variances.append([])
+		z_variances.append([])
+		for j in range(len(binnedData[i])):
+			val = binnedData[i][j]
+			if len(val) > 0 and len(val[0]) > 0:
+				x_variances[i].append(calcvariance(val[0]))
+				y_variances[i].append(calcvariance(val[1]))
+				z_variances[i].append(calcvariance(val[2]))
+			else:
+				x_variances[i].append(0)
+				y_variances[i].append(0)
+				z_variances[i].append(0)
+
+	x_variances = np.asarray(x_variances)
+	y_variances = np.asarray(y_variances)
+	z_variances = np.asarray(z_variances)
+
+	return zbins, xbins, x_variances, y_variances, z_variances
+
 def energy_fluctuation(x,y,z,vx,vy,vz, zmin, zstep):
 	'''Returns a 2D array, where each row gives a z value and variance in x,y and z velocity'''
 	data = sorted(list(zip(x,y,z,vx,vy,vz)), key=lambda x: x[2])
@@ -49,13 +114,13 @@ def calcvariance(velocities):
 
 def plot_variances(filename):
 	_,x,y,z,_,vx,vy,vz,_,_,_ = read_file(filename)
-	zsteps, x_variance, y_variance, z_variance = energy_fluctuation(x,y,z,vx,vy,vz,min(z),1)
+	zbins, xbins, x_variances, y_variances, z_variances = twoD_binned_energy_fluctuations(x,y,z,vx,vy,vz,min(z),1, min(x),1)
 
 	plt.figure()
-	plt.plot(zsteps, x_variance, label="x")
-	plt.plot(zsteps, y_variance, label="y")
-	plt.plot(zsteps, z_variance, label="z")
-	plt.legend()
+	plt.contour(xbins, zbins,x_variances)
+	plt.colorbar()
+	plt.xlim(xbins[0], xbins[-1])
+	plt.ylim(zbins[0], zbins[-1])
 	plt.show()
 
 if __name__ == '__main__':
